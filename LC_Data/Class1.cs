57382e59b45ecaf1494f6e19cfa28bf5b1410038
Ruan <ruangbarros@gmail.com>
2021-06-02 00:08:09 -0300

    adding Migration

--- LC_Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LC_Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ LC_Data/Context/LCContext.cs
@@ -0,0 +1,15 @@
+﻿using LC_Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace LC_Data
+{
+    public class LCContext : DbContext
+    {
+        public DbSet<Customer> Customers {get; set;}
+
+        public LCContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
