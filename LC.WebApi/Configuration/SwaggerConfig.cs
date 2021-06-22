34d15e99f645484d3a5598f513986ba4ee342d99
Ruan <ruangbarros@gmail.com>
2021-06-21 23:19:00 -0300

    adding Xml documentation

--- LC.WebApi/Configuration/SwaggerConfig.cs
+++ LC.WebApi/Configuration/SwaggerConfig.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace LC.WebApi.Configuration
 {
@@ -9,25 +11,32 @@ namespace LC.WebApi.Configuration
     {
         public static void AddSwaggerConfiguration(this IServiceCollection services)
         {
-            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
-            { 
-                Title = "Consultório Legal",
-                Version = "v1",
-                Description = "API da aplicação Consultório Legal",
-                Contact = new OpenApiContact
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                new OpenApiInfo
                 {
-                    Name = "Ruan Barros",
-                    Email = "ruangbarros@gmail.com",
-                    Url = new Uri("https://github.com/ruangb")
-                },
-                License = new OpenApiLicense
-                {
-                    Name = "OSD",
-                    Url = new Uri("https://opensource.org/osd")
-                },
-                TermsOfService = new Uri("https://opensource.org/osd")
-            }
-            ));  
+                    Title = "Consultório Legal",
+                    Version = "v1",
+                    Description = "API da aplicação Consultório Legal",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Ruan Barros",
+                        Email = "ruangbarros@gmail.com",
+                        Url = new Uri("https://github.com/ruangb")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "OSD",
+                        Url = new Uri("https://opensource.org/osd")
+                    },
+                    TermsOfService = new Uri("https://opensource.org/osd")
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         public static void UseSwaggerConfiguration(this IApplicationBuilder app)
