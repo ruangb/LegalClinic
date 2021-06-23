4633d44c68c5a9b730906e826b54f3318fa3b2fb
Ruan <ruangbarros@gmail.com>
2021-06-22 21:34:21 -0300

    adding comments to controller

--- LC.WebApi/Controllers/CustomersController.cs
+++ LC.WebApi/Controllers/CustomersController.cs
@@ -1,5 +1,4 @@
 ﻿using LC.Core.Shared.ModelViews;
-using LC.Core;
 using LC.Manager.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -17,18 +16,29 @@ namespace LC.WebApi.Controllers
             this.customerManager = customerManager;
         }
 
+        /// <summary>
+        /// Return all the customers in the database
+        /// </summary>
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             return Ok(await customerManager.GetCustomersAsync());
         }
 
+        /// <summary>
+        /// return a customer searched by id
+        /// </summary>
+        /// <param name="id" example="1">customer id</param>
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(int id)
         {
             return Ok(await customerManager.GetCustomerAsync(id));
         }
 
+        /// <summary>
+        /// Insert a new customer
+        /// </summary>
+        /// <param name="newCustomer">a new customer to insert</param>
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] NewCustomer newCustomer)
         {
@@ -37,6 +47,10 @@ namespace LC.WebApi.Controllers
             return CreatedAtAction(nameof(Get), new { id = insertedCustomer.Id }, insertedCustomer);
         }
         
+        /// <summary>
+        /// update a customer
+        /// </summary>
+        /// <param name="updateCustomer">custober to be updated</param>
         [HttpPut]
         public async Task<IActionResult> Put([FromBody] UpdateCustomer updateCustomer)
         {
@@ -48,6 +62,10 @@ namespace LC.WebApi.Controllers
             return Ok(updatedCustomer);
         }
  
+        /// <summary>
+        /// delete customer by id
+        /// </summary>
+        /// <param name="id">customer id</param>
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
