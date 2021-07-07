36236b66a010997452c60239b2ea979bec8a2264
Ruan <ruangbarros@gmail.com>
2021-07-06 22:27:41 -0300

    complementing CustomerConfiguration

--- LC_Data/Configuration/CustomerConfiguration.cs
+++ LC_Data/Configuration/CustomerConfiguration.cs
@@ -9,6 +9,8 @@ namespace LC.Data.Configuration
         public void Configure(EntityTypeBuilder<Customer> builder)
         {
             builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).HasMaxLength(200).IsRequired();
+            builder.Property(p => p.Gender).HasDefaultValue('M').IsRequired();
         }
     }
 }
