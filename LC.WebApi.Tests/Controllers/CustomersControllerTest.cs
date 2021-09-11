7024fe50d528dbe226fb540ca67d7a0553ec1107
Ruan <ruangbarros@gmail.com>
2021-09-11 13:45:12 -0300

    adding CustomerControllerTest

---
+++ LC.WebApi.Tests/Controllers/CustomersControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using LC.Manager.Implementation;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LC.WebApi.Tests.Controllers
+{
+    public class CustomersControllerTest
+    {
+        private readonly CustomerManager manager;
+
+        public CustomersControllerTest()
+        {
+
+        }
+
+        [Fact]
+        public async Task Get_Ok()
+        {
+
+        }
+    }
+}
